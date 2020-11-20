34c5f8911e454f91066721ba27b5856ad1ad1fe0
Philipp Gühring <pg@futureware.at>
2020-11-20 18:17:40 +0100

    Improved build log

--- openlane/build.pl
+++ openlane/build.pl
@@ -15,15 +15,16 @@ foreach(<*>)
   print OUT "<h2>Submodule: $_</h2>\n<pre>";
   my $res=`make $_ 2>&1`;
   $res=~s/\&/&amp;/g;
-  $res=~s/\x1b\[31m/<div style="background:#ff8080">/g;
-  $res=~s/\x1b\[36m/<div style="background:#c0c0ff">/g;
+  $res=~s/\r//g;
+  $res=~s/\x1b\[31m/<div style="background:#ff8080;display:inline">/g;
+  $res=~s/\x1b\[33m/<div style="background:#c0ffc0;display:inline">/g;
+  $res=~s/\x1b\[36m/<div style="background:#c0c0ff;display:inline">/g;
   $res=~s/\x1b\[37m/<\/div>/g;
-
-  $res=~s/(.*success.*)/<div style="background:#80ff80">$1<\/div>/ig;
-  $res=~s/(.*finish.*)/<div style="background:#80ff80">$1<\/div>/ig;
-  $res=~s/(.*error.*)/<div style="background:#ff8080">$1<\/div>/ig;
-  $res=~s/(.*fail.*)/<div style="background:#ff8080">$1<\/div>/ig;
-  $res=~s/(.*warn.*)/<div style="background:#ff8080">$1<\/div>/ig;
+  $res=~s/(.*success.*)/<div style="background:#80ff80;display:inline">$1<\/div>/ig;
+  $res=~s/(.*finish.*)/<div style="background:#80ff80;display:inline">$1<\/div>/ig;
+  $res=~s/(.*error.*)/<div style="background:#ff8080;display:inline">$1<\/div>/ig;
+  $res=~s/(.*fail.*)/<div style="background:#ff8080;display:inline">$1<\/div>/ig;
+  $res=~s/(.*warn.*)/<div style="background:#ff8080;display:inline">$1<\/div>/ig;
 
 
   print OUT $res;
