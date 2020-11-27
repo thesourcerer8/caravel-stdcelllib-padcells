c51340fa7728eda62dba55552370fe03e4cee0f1
Philipp Gühring <pg@futureware.at>
2020-11-27 14:59:23 +0100

    "make shell" now sets all environment variables from the previous run

---
+++ openlane/env.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+open IN,"<user_proj_example/runs/user_proj_example/config.tcl";
+open OUT,">config.sh";
+while(<IN>)
+{
+  s/^set\s+::env\(([^)]+)\)\s+([\w.\-\/]+)/export $1="$2"/;
+  s/^set\s+::env\(([^)]+)\)\s+"([^"]+)"/export $1="$2"/;
+
+  print OUT $_;
+}
+close IN;
+close OUT;
+print "config.sh has been written.\n";
