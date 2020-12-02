e91d51ac48f1489a609721fcd265dfdd1d56dfce
Philipp Gühring <pg@futureware.at>
2020-12-03 00:31:54 +0100

    Improved the environment variable conversion

--- openlane/env.pl
+++ openlane/env.pl
@@ -3,8 +3,9 @@ open IN,"<user_proj_example/runs/user_proj_example/config.tcl";
 open OUT,">config.sh";
 while(<IN>)
 {
-  s/^set\s+::env\(([^)]+)\)\s+([\w.\-\/]+)/export $1="$2"/;
-  s/^set\s+::env\(([^)]+)\)\s+"([^"]+)"/export $1="$2"/;
+  s/^set\s+::env\(([^) ]+)\)\s+([\w.\-\/]+)/export $1="$2"/;
+  s/^set\s+::env\(([^) ]+)\)\s+"([^"]+)"/export $1="$2"/;
+  s/^set\s+::env\(([^) ]+)\)\s+(\[.*?\])/export $1="$2"/;
 
   print OUT $_;
 }
