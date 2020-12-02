d974ae9f618691bd1b56363e91696a361a87fce3
Philipp Gühring <pg@futureware.at>
2020-12-03 00:33:50 +0100

    Improved the config

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -4,8 +4,8 @@ set ::env(DESIGN_NAME) user_proj_example
 
 set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_proj_example.v \
-	$script_dir/../../verilog/rtl/user_proj_cells.v"
+	$script_dir/../../verilog/rtl/user_proj_example.v"
+#	$script_dir/../../verilog/rtl/user_proj_cells.v"
 
 
 
@@ -30,7 +30,7 @@ set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 #set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 600 600"
+set ::env(DIE_AREA) "0 0 300 300"
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
@@ -47,13 +47,13 @@ set ::env(FP_CORE_UTIL) 5
 #set ::env(FP_PDN_HOFFSET) 4
 #set ::env(FP_PDN_HPITCH) 15
 
-set ::env(CLOCK_PORT) 0
+set ::env(CLOCK_PORT) "io_in\[10\]"
 set ::env(CLOCK_PERIOD) 0
 set ::env(CLOCK_TREE_SYNTH) 0
 
 set ::env(PL_RANDOM_GLB_PLACEMENT) 1
 
-set ::env(PL_TARGET_DENSITY) 0.55
+set ::env(PL_TARGET_DENSITY) 8.55
 set ::env(PL_RANDOM_GLB_PLACEMENT) 0
 set ::env(PL_BASIC_PLACEMENT) 0
 set ::env(PL_SKIP_INITIAL_PLACEMENT) 0
