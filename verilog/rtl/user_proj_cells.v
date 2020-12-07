2540d45df301e1528eb6bf1aebe2c5ff5d47cb12
Philipp Gühring <pg@futureware.at>
2020-12-07 02:20:33 +0100

    Update

--- verilog/rtl/user_proj_cells.v
+++ verilog/rtl/user_proj_cells.v
@@ -1,131 +1,236 @@
 `default_nettype none
+
 /*
  *-------------------------------------------------------------
  *
- * user_proj_ls130tw1  (LibreSilicon Testwafer #1)
+ * user_proj_cells  (LibreSilicon Testwafer #1)
  *
  */
 
 module AND2X1(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
 module AND2X2(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
-);
-endmodule 
-
-module OR2X1
-(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
-module OR2X2
-(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
+module AOI21X1(
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
-module NOR2X1
-(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
+module AOI22X1(
+  inout D, // input
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
-module NOR2X2
-(
-	inout vdd,
-	inout gnd,
-	inout B,
-	inout A,
-	inout Y
+module BUFX2(
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module BUFX4(
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module CLKBUF1(
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module HAX1(
+  inout B, // input
+  inout A, // input
+  inout YS, // output
+  inout YC, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
 module INV(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
 module INVX1(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
+
 module INVX2(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
-module INVX3(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
-);
-endmodule
+
 module INVX4(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
-module BUFX1(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
-);
-endmodule
-module BUFX2(
-	inout vdd,
-	inout gnd,
-	inout A,
-	inout Y
+module INVX8(
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
-module AOI21X1
-(
-	inout vdd,
-	inout gnd,
-	inout C,
-	inout B,
-	inout A,
-	inout Y
+module LATCH(
+  inout D, // input
+  inout CLK, // input
+  inout Q, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
 );
 endmodule
 
+module MUX2X1(
+  inout S, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
 
+module NAND2X1(
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module NAND3X1(
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module NOR2X1(
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module NOR3X1(
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module OAI21X1(
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module OAI22X1(
+  inout D, // input
+  inout C, // input
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module OR2X1(
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module OR2X2(
+  inout B, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module TBUFX1(
+  inout EN, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
+
+module TBUFX2(
+  inout EN, // input
+  inout A, // input
+  inout Y, // output
+	inout vdd, // cell power supply
+	inout gnd  // cell ground supply
+);
+endmodule
 
