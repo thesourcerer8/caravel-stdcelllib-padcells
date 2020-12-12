089201dff2ae568cda41a153b1c2fcb234ec253e
Philipp Gühring <pg@futureware.at>
2020-12-12 21:29:56 +0100

    Fixed file handling

--- scripts/placement.pl
+++ scripts/placement.pl
@@ -9,6 +9,7 @@ foreach my $mag(<$STDCELLLIB/Catalog/*.mag>)
   next if((-s $mag)<=50);
   #print `ls -la $mag`;
   my $cell=$mag; $cell=~s/\.mag$/.cell/;
+  next unless(-f $cell);
   my $name=""; $name=$1 if($mag=~m/([\w\-\.]+)\.mag$/);
   print "$name ".(46-0.46*12)." $nextla N\n";
   $nextla+=2.72*2;
