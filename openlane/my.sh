3c0d70c3c3696565e788285148f0d23b67fdecfe
Philipp Gühring <pg@futureware.at>
2020-12-14 14:36:31 +0100

    Improved the build script

--- openlane/my.sh
+++ openlane/my.sh
@@ -3,6 +3,8 @@ export OPENLANE_IMAGE_NAME=openlane:rc5
 export OPENLANE_ROOT=/media/philipp/Daten/skywater/openlane/
 export PDK_ROOT=/media/philipp/Daten/skywater/open_pdks/sky130
 make user_proj_example
+cp user_proj_example/runs/user_proj_example/results/magic/user_proj_example.mag ../mag/user_proj_example.mag
 make user_project_wrapper
 cd ..
-make
+make ship
+make compress
